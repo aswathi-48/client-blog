9111bc24dba868fdf3ab0e881eb5507c4b9f725e
aswathi-48 <aswathik482@gmail.com>
2026-05-30 14:29:30 +0530

    register completed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Register.css";
+import axios from 'axios'
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    phone: "",
+  });
 
-export default Register
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/user/register",
+        formData
+      );
+
+      alert(res.data.message);
+
+      setFormData({
+        name: "",
+        email: "",
+        password: "",
+        phone: "",
+      });
+    } catch (err) {
+      console.log(err);
+      alert("Registration Failed");
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <form className="register-card" onSubmit={handleSubmit}>
+        <h2>Create Account</h2>
+
+        <input
+          type="text"
+          name="name"
+          placeholder="Full Name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email Address"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <input
+          type="tel"
+          name="phone"
+          placeholder="Phone Number"
+          value={formData.phone}
+          onChange={handleChange}
+          required
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
